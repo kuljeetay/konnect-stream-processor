bc02aab2fbb236854a22b91c8788a2120d3e7449
Kuljeet Kumar <ckj3256@gmail.com>
2024-07-26 15:49:45 +0530

    Minor Changes

--- src/consumer/main.py
+++ src/consumer/main.py
@@ -5,20 +5,21 @@ from config import KAFKA_CONFIG, KAFKA_TOPIC
 
 
 def main():
-    configure_logging()
-    logging.info("Starting Konnect Stream Consumer...")
+    try:
+        configure_logging()
+        logging.info("Starting Konnect Stream Consumer...")
 
-    consumer = KonnectStreamConsumer(KAFKA_CONFIG)
-    logging.info(
-        "Konnect Stream Consumer initialized : "
-        + str(consumer.is_initalized(topic=KAFKA_TOPIC))
-    )
+        consumer = KonnectStreamConsumer(KAFKA_CONFIG)
+        logging.info(
+            "Konnect Stream Consumer initialized : "
+            + str(consumer.is_initalized(topic=KAFKA_TOPIC))
+        )
 
-    consumer.subscribe(topic=KAFKA_TOPIC)
+        consumer.subscribe(topic=KAFKA_TOPIC)
 
-    # setup_signal_handlers(consumer)
-
-    consumer.consume_messages()
+        consumer.consume_messages()
+    except Exception as ex:
+        logging.exception(str(ex))
 
 
 if __name__ == "__main__":
--- src/consumer/utils.py
+++ src/consumer/utils.py
@@ -1,6 +1,5 @@
 import logging
 import sys
-import signal
 from config import LOGGING_CONFIG
 
 
@@ -14,16 +13,3 @@ def configure_logging():
         ],
     )
 
-
-def shutdown_hook(consumer):
-    def shutdown(signum, frame):
-        logging.info("Shutting down consumer...")
-        consumer.consumer.close()
-        sys.exit(0)
-
-    return shutdown
-
-
-# def setup_signal_handlers(consumer):
-#     signal.signal(signal.SIGINT, shutdown_hook(consumer))
-#     signal.signal(signal.SIGTERM, shutdown_hook(consumer))
